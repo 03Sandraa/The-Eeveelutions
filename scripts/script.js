0dc756a8649c146b3ce68b7757762c4823d0cfbf
03Sandraa <90039351+03Sandraa@users.noreply.github.com>
2022-11-10 17:20:12 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -7,9 +7,9 @@ const FireStone = document.querySelector(ShortHandStones + "li:nth-of-type(2)");
 const WaterStone = document.querySelector(ShortHandStones + "li:nth-of-type(3)");
 const LeafStone = document.querySelector(ShortHandStones + "li:nth-of-type(4)");
 const IceStone = document.querySelector(ShortHandStones + "li:nth-of-type(5)");
-const PsychicEvolveWay = document.querySelector(ShortHandStones + "li:nth-of-type(6)");
-const DarkEvolveWay = document.querySelector(ShortHandStones + "li:nth-of-type(7)");
-const FairyEvolveWay = document.querySelector(ShortHandStones + "li:nth-of-type(8)");
+const FairyAttack = document.querySelector(ShortHandStones + "li:nth-of-type(6)");
+const GiveLove = document.querySelector(ShortHandStones + "li:nth-of-type(7)");
+const LevelUp = document.querySelector(ShortHandStones + "li:nth-of-type(8)");
 
 const Dragzone = document.querySelector("section:first-of-type");
 
@@ -43,19 +43,19 @@ IceStone.addEventListener("dragstart", (event) => {
     dragged = event.target;
 });
 
-PsychicEvolveWay.addEventListener("dragstart", (event) => {
+FairyAttack.addEventListener("dragstart", (event) => {
     event.dataTransfer.clearData();
     event.dataTransfer.setData("text", event.target.id);
     dragged = event.target;
 });
 
-DarkEvolveWay.addEventListener("dragstart", (event) => {
+GiveLove.addEventListener("dragstart", (event) => {
     event.dataTransfer.clearData();
     event.dataTransfer.setData("text", event.target.id);
     dragged = event.target;
 });
 
-FairyEvolveWay.addEventListener("dragstart", (event) => {
+LevelUp.addEventListener("dragstart", (event) => {
     event.dataTransfer.clearData();
     event.dataTransfer.setData("text", event.target.id);
     dragged = event.target;
@@ -66,37 +66,103 @@ Dragzone.addEventListener("dragover", (event) => {
     event.preventDefault();
 });
 
+let LoveStatus = 0;
+let FairyAttackStatus = 0;
+const PokemonName = document.querySelector("h1");
+
 Dragzone.addEventListener("drop", (event) => {
     event.preventDefault();
     const data = event.dataTransfer.getData("text")
     const Page = document.querySelector("section:first-of-type");
-    console.log(event.dataTransfer.getData("text"));
+    const EvolveAnimation = document.querySelector("section:first-of-type figure:nth-of-type(2) div");
+
 
     if (data == "ThunderStone") {
-        Page.classList.add("Evolve", "Jolteon");
+        EvolveAnimation.classList.add("Evolve");
+        Page.classList.add("Evolve");
+        EvolveAnimation.addEventListener('animationend', () => {
+            Page.classList.add("Jolteon");
+            PokemonName.innerText = "Jolteon";
+          });
     }
     else if (data == "FireStone") {
-        Page.classList.add("Evolve", "Flareon");
+        EvolveAnimation.classList.add("Evolve");
+        Page.classList.add("Evolve");
+        EvolveAnimation.addEventListener('animationend', () => {
+            Page.classList.add("Flareon");
+            PokemonName.innerText = "Flareon";
+          });
     }
     else if (data == "WaterStone") {
-        Page.classList.add("Evolve", "Vaporeon");
+        EvolveAnimation.classList.add("Evolve");
+        Page.classList.add("Evolve");
+        EvolveAnimation.addEventListener('animationend', () => {
+            Page.classList.add("Vaporeon");
+            PokemonName.innerText = "Vaporeon";
+          });
     }
     else if (data == "LeafStone") {
-        Page.classList.add("Evolve", "Leafeon");
+        EvolveAnimation.classList.add("Evolve");
+        Page.classList.add("Evolve");
+        EvolveAnimation.addEventListener('animationend', () => {
+            Page.classList.add("Leafeon");
+            PokemonName.innerText = "Leafeon";
+          });
+    }
+    else if (data == "LevelUp" && Location == "Forest") {
+        EvolveAnimation.classList.add("Evolve");
+        Page.classList.add("Evolve");
+        EvolveAnimation.addEventListener('animationend', () => {
+            Page.classList.add("Leafeon");
+            PokemonName.innerText = "Leafeon";
+          });
     }
     else if (data == "IceStone") {
-        Page.classList.add("Evolve", "Glaceon");
+        EvolveAnimation.classList.add("Evolve");
+        Page.classList.add("Evolve");
+        EvolveAnimation.addEventListener('animationend', () => {
+            Page.classList.add("Glaceon");
+            PokemonName.innerText = "Glaceon";
+          });
     }
-    else if (data == "PsychicEvolveWay") {
-        Page.classList.add("Evolve", "Espeon");
+    else if (data == "LevelUp" && Location == "Snow") {
+        EvolveAnimation.classList.add("Evolve");
+        Page.classList.add("Evolve");
+        EvolveAnimation.addEventListener('animationend', () => {
+            Page.classList.add("Glaceon");
+            PokemonName.innerText = "Glaceon";
+          });
     }
-    else if (data == "DarkEvolveWay") {
-        Page.classList.add("Evolve", "Umbreon");
+    else if (data == "LevelUp" && LoveStatus == 1 && Location == "Day") {
+        EvolveAnimation.classList.add("Evolve");
+        Page.classList.add("Evolve");
+        EvolveAnimation.addEventListener('animationend', () => {
+            Page.classList.add("Espeon");
+            PokemonName.innerText = "Espeon";
+          });
     }
-    else if (data == "FairyEvolveWay") {
-        Page.classList.add("Evolve", "Sylveon");
+    else if (data == "LevelUp" && LoveStatus == 1 && Location == "Night") {
+        EvolveAnimation.classList.add("Evolve");
+        Page.classList.add("Evolve");
+        EvolveAnimation.addEventListener('animationend', () => {
+            Page.classList.add("Umbreon");
+            PokemonName.innerText = "Umbreon";
+          });
     }
-    
+    else if (data == "FairyAttack") {
+        FairyAttackStatus = 1;
+    }
+    else if (data == "Love") {
+        LoveStatus = 1;
+    }
+    else if (data == "LevelUp" && LoveStatus == 1 && FairyAttackStatus == 1) {
+        EvolveAnimation.classList.add("Evolve");
+        Page.classList.add("Evolve");
+        EvolveAnimation.addEventListener('animationend', () => {
+            Page.classList.add("Sylveon");
+            PokemonName.innerText = "Sylveon";
+          });
+    }
 });
 
 // Location
@@ -106,24 +172,30 @@ const ButtonMoonLocation = document.querySelector("header ul li:nth-of-type(2) b
 const ButtonGrassLocation = document.querySelector("header ul li:nth-of-type(3) button");
 const ButtonIceLocation = document.querySelector("header ul li:nth-of-type(4) button");
 
+let Location = "Base";
+
 ButtonSunLocation.addEventListener("click", () => {
     body.classList.add("Sunny");
     body.classList.remove("Dark", "Grass", "Ice");
+    Location = "Day";
 });
 
 ButtonMoonLocation.addEventListener("click", () => {
     body.classList.add("Dark");
     body.classList.remove("Sunny", "Grass", "Ice");
+    Location = "Night";
 });
 
 ButtonGrassLocation.addEventListener("click", () => {
     body.classList.add("Grass");
     body.classList.remove("Sunny", "Dark", "Ice");
+    Location = "Forest";
 });
 
 ButtonIceLocation.addEventListener("click", () => {
     body.classList.add("Ice");
     body.classList.remove("Sunny", "Dark", "Grass");
+    Location = "Snow";
 });
 
 // Shiny
@@ -139,4 +211,37 @@ const ButtonReset = document.querySelector("header ul li:nth-of-type(6) button")
 
 ButtonReset.addEventListener("click", () => {
     window.location.reload();
+});
+
+const HiddenCheatSheet = document.querySelector("section:nth-of-type(2)");
+let TimeClicksCheatsheet = 0;
+const CheatSheetBackdrop = document.querySelector("body > div");
+
+CheatSheetBackdrop.addEventListener("click", () => {
+        HiddenCheatSheet.classList.remove("Open");
+        CheatSheetBackdrop.classList.remove("Open");
+        body.classList.remove("Overlay");
+        TimeClicksCheatsheet = 0;
+});
+
+HiddenCheatSheet.addEventListener("click", () => {
+    if (TimeClicksCheatsheet == 0){
+        HiddenCheatSheet.classList.add("Stage1");
+        TimeClicksCheatsheet = 1;
+    } else if (TimeClicksCheatsheet == 1){
+        HiddenCheatSheet.classList.add("Stage2");
+        HiddenCheatSheet.classList.remove("Stage1");
+        TimeClicksCheatsheet = 2;
+    } else if (TimeClicksCheatsheet == 2){
+        HiddenCheatSheet.classList.add("Open");
+        CheatSheetBackdrop.classList.add("Open");
+        body.classList.add("Overlay");
+        HiddenCheatSheet.classList.remove("Stage2");
+        TimeClicksCheatsheet = 3;
+    } else if (TimeClicksCheatsheet == 3){
+        HiddenCheatSheet.classList.remove("Open");
+        CheatSheetBackdrop.classList.remove("Open");
+        body.classList.remove("Overlay");
+        TimeClicksCheatsheet = 0;
+    }
 });
